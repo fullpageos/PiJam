3aeeabf2017b8716f3f13b5fb6a2d4431ba7cce4
Tailor Vijay <tailor.vj@gmail.com>
2019-07-25 18:01:49 +0100

    cleaned desktop, using screen to run browser, installed and using unclutter, running experimental shell scripts, added branding

--- src/modules/pijam/filesystem/home/pi/scripts/run_pijam.sh
+++ src/modules/pijam/filesystem/home/pi/scripts/run_pijam.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-cd /home/pi/electron/fullpagedashboard-client
-npm start > /tmp/electron.log 2>&1
+
+# Replace desktop backgroud with FullPageOS logo
+export DISPLAY=:0
+pcmanfm --set-wallpaper="/home/pi/Pictures/lxde-desktop-background-fullpageos-logo-blackbg-1920x1080.png"
+
+# Remove cursor from desktop
+unclutter -display :0 -noevents -grab
+
+# Run fullpagedashboard client experimental auto update scripts
+cd /home/pi/electron/fullpageos-experimental-shell-scripts
+./update
+
+#cd /home/pi/electron/fullpagedashboard-client
+screen -d -m electron /home/pi/electron/fullpagedashboard-client/main.js > /tmp/electron.log 2>&1
